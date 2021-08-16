463496e4477b1bdc8a58dcb9d0662a82a7eeab46
andrescol24 <andresmil24@hotmail.com>
2021-08-16 13:31:11 -0500

    Logica de creacion y consulta

--- src/main/java/co/andresmorales/leantech/turismo/reservasapi/rest/ReservasRest.java
+++ src/main/java/co/andresmorales/leantech/turismo/reservasapi/rest/ReservasRest.java
@@ -1,37 +1,54 @@
 package co.andresmorales.leantech.turismo.reservasapi.rest;
 
-import javax.websocket.server.PathParam;
-
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.andresmorales.leantech.turismo.reservasapi.dto.Reserva;
+import co.andresmorales.leantech.turismo.reservasapi.dto.ReservaCreada;
+import co.andresmorales.leantech.turismo.reservasapi.service.ReservasService;
 
 /**
  * Rest para recibir las peticiones HTTP para las reservas
+ * 
  * @author Andres Morales
  *
  */
 @RestController
 public class ReservasRest {
-	
+
+	private ReservasService servicio;
+
+	@Autowired
+	public ReservasRest(ReservasService servicio) {
+		this.servicio = servicio;
+	}
+
 	/**
 	 * Permite consultar una reserva
+	 * 
 	 * @param id ID de la reserva
 	 * @return Reserva
 	 */
 	@GetMapping("/consultar-reserva/{id}")
-	public ResponseEntity<Reserva> consultarReserva(@PathParam("id") Integer id) {
-		return new ResponseEntity<>(HttpStatus.OK);
+	public ResponseEntity<Reserva> consultarReserva(@PathVariable("id") Integer id) {
+		return this.servicio.consultarReserva(id);
 	}
-	
+
+	/**
+	 * Permite crear una reserva
+	 * 
+	 * @param reserva Reserva a crear
+	 * @return Respose con el HTTP status y el ID de la reserva creada en caso de
+	 *         ser creada
+	 */
 	@PostMapping("/crear-reserva")
-	public ResponseEntity<String> crearReserva(@RequestBody Reserva reserva) {
-		return new ResponseEntity<>("Creando Reserva 5", HttpStatus.OK);
+	public ResponseEntity<ReservaCreada> crearReserva(@RequestBody Reserva reserva) {
+		return this.servicio.crearReserva(reserva);
 	}
-	
+
 }
--- src/main/java/co/andresmorales/leantech/turismo/reservasapi/service/ReservasService.java
+++ src/main/java/co/andresmorales/leantech/turismo/reservasapi/service/ReservasService.java
@@ -1,9 +1,17 @@
 package co.andresmorales.leantech.turismo.reservasapi.service;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import co.andresmorales.leantech.turismo.reservasapi.dto.Reserva;
+import co.andresmorales.leantech.turismo.reservasapi.dto.ReservaCreada;
+import co.andresmorales.leantech.turismo.reservasapi.model.ReservaModel;
 import co.andresmorales.leantech.turismo.reservasapi.repository.ReservaRepository;
 
 /**
@@ -22,13 +30,39 @@ public class ReservasService {
 	public ReservasService(ReservaRepository reservaRepository) {
 		this.reservaRepository = reservaRepository;
 	}
-	
+
 	/**
 	 * Permite crear una reserva
+	 * 
 	 * @param reserva Reserva a crear
-	 * @return Reserva creada
+	 * @return {@link ResponseEntity} con el ID asignado
 	 */
-	public Reserva crearReserva(Reserva reserva) {
-		return null;
+	public ResponseEntity<ReservaCreada> crearReserva(Reserva reserva) {
+		if (reserva.getFechaIngreso() == null || reserva.getFechaSalida() == null
+				|| reserva.getTitularReserva() == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		ReservaModel modelo = new ReservaModel(reserva);
+		Logger log = LoggerFactory.getLogger(getClass());
+		this.reservaRepository.save(modelo);
+		log.info("Se creo la reserva #{}, entre {} y {} para {}", modelo.getId(), modelo.getFechaIngreso(),
+				modelo.getFechaIngreso(), modelo.getTitularReserva());
+		return new ResponseEntity<>(new ReservaCreada(modelo.getId()), HttpStatus.OK);
+	}
+
+	/**
+	 * Consulta una reserva por el ID
+	 * 
+	 * @param id Id de la reserva
+	 * @return {@link ResponseEntity} con la reserva consultada. En caso de no ser
+	 *         encontrada el ResponseEntity tendra un {@link HttpStatus#NOT_FOUND}
+	 */
+	public ResponseEntity<Reserva> consultarReserva(int id) {
+		Optional<ReservaModel> reservaModel = this.reservaRepository.findById(id);
+		if (reservaModel.isPresent()) {
+			return new ResponseEntity<>(new Reserva(reservaModel.get()), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 }
