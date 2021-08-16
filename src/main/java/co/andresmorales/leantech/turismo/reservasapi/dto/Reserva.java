1f0551d8a556988bc79d34df513e0de354b82541
andrescol24 <andresmil24@hotmail.com>
2021-08-16 12:55:02 -0500

    Creo constructores

--- src/main/java/co/andresmorales/leantech/turismo/reservasapi/dto/Reserva.java
+++ src/main/java/co/andresmorales/leantech/turismo/reservasapi/dto/Reserva.java
@@ -2,11 +2,12 @@ package co.andresmorales.leantech.turismo.reservasapi.dto;
 
 import java.util.Date;
 
+import co.andresmorales.leantech.turismo.reservasapi.model.ReservaModel;
 import lombok.Getter;
 import lombok.Setter;
 
 /**
- * Esta clase representa una reserva realizada por un usuario
+ * DTO que representa una reserva realizada por un usuario
  * 
  * @author Andres Morales
  *
@@ -16,32 +17,43 @@ public class Reserva {
 	@Getter
 	@Setter
 	private int id;
-	
+
 	@Getter
 	@Setter
 	private Date fechaIngreso;
-	
+
 	@Getter
 	@Setter
 	private Date fechaSalida;
-	
+
 	@Getter
 	@Setter
 	private short totalDias;
-	
+
 	@Getter
 	@Setter
 	private short numeroPersonas;
-	
+
 	@Getter
 	@Setter
 	private String titularReserva;
-	
+
 	@Getter
 	@Setter
 	private short numeroHabitaciones;
-	
+
 	@Getter
 	@Setter
 	private short numeroMenores;
+
+	public Reserva(ReservaModel reservaModel) {
+		this.id = reservaModel.getId();
+		this.fechaIngreso = reservaModel.getFechaIngreso();
+		this.fechaSalida = reservaModel.getFechaSalida();
+		this.totalDias = reservaModel.getTotalDias();
+		this.numeroPersonas = reservaModel.getNumeroPersonas();
+		this.titularReserva = reservaModel.getTitularReserva();
+		this.numeroHabitaciones = reservaModel.getNumeroHabitaciones();
+		this.numeroMenores = reservaModel.getNumeroMenores();
+	}
 }
--- src/main/java/co/andresmorales/leantech/turismo/reservasapi/model/ReservaModel.java
+++ src/main/java/co/andresmorales/leantech/turismo/reservasapi/model/ReservaModel.java
@@ -1,12 +1,13 @@
 package co.andresmorales.leantech.turismo.reservasapi.model;
 
-import java.util.Date;
+import java.sql.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import co.andresmorales.leantech.turismo.reservasapi.dto.Reserva;
 import lombok.Data;
 
 /**
@@ -17,18 +18,31 @@ import lombok.Data;
  */
 @Entity
 @Data
-@SuppressWarnings("unused")
 public class ReservaModel {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	private Date fechaIngreso;
-	private Date fechaSalida;
+	private Timestamp fechaIngreso;
+	private Timestamp fechaSalida;
 	private short totalDias;
 	private short numeroPersonas;
 	private String titularReserva;
 	private short numeroHabitaciones;
 	private short numeroMenores;
+	
+	/**
+	 * Crea una instancia del modelo sin el ID ya que es asignado por la base de datos
+	 * @param reserva DTO recibido
+	 */
+	public ReservaModel(Reserva reserva) { 
+		this.fechaIngreso = new Timestamp(reserva.getFechaIngreso().getTime());
+		this.fechaSalida = new Timestamp(reserva.getFechaSalida().getTime());
+		this.totalDias = reserva.getTotalDias();
+		this.numeroPersonas = reserva.getNumeroPersonas();
+		this.titularReserva = reserva.getTitularReserva();
+		this.numeroHabitaciones = reserva.getNumeroHabitaciones();
+		this.numeroMenores = reserva.getNumeroMenores();
+	}
 }
